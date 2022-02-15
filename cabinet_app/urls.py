d02a55497112ffc641b374a5bf22bfbbc1f68ea8
revike <revike@ya.ru>
2022-02-15 16:14:35 +0100

    cabinet start

--- cabinet_app/urls.py
+++ cabinet_app/urls.py
@@ -4,5 +4,5 @@ from cabinet_app import views as cab_app
 app_name = 'cabinet_app'
 
 urlpatterns = [
-    path('', cab_app.CabinetIndexView.as_view(), name='index_cab'),
+    path('', cab_app.CabinetIndexView.as_view(), name='profile'),
 ]
--- cabinet_app/views.py
+++ cabinet_app/views.py
@@ -1,20 +1,40 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import TemplateView
+from django.views.generic import CreateView
 
+from auth_app.models import CompanyUserProfile
+from cabinet_app.forms import CompanyProfileCreateForm
 from main_app.models import ProductCategory
 
 
-class CabinetIndexView(TemplateView):
+class CabinetIndexView(CreateView):
     """Контроллер главной страница личного кабинета"""
-    template_name = 'cabinet_app/index.html'
+    template_name = 'cabinet_app/profile.html'
+    form_class = CompanyProfileCreateForm
 
     def get_context_data(self, **kwargs):
         """Возвращает контекст для этого представления"""
         context = super().get_context_data(**kwargs)
+        company_id = self.request.user.id
+        company = CompanyUserProfile.objects
         context['title'] = 'личный кабинет'
         context['categories'] = ProductCategory.get_categories()
+        context['company'] = company.filter(
+            company__id=company_id, is_active=True,
+            company__is_active=True).first()
+        context['company_not_active'] = company.filter(
+            company__id=company_id, is_active=False, company__is_active=True)
         return context
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(self.request.POST, self.request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.company = self.request.user
+            form.save()
+        return HttpResponseRedirect(reverse('cab_app:profile'))
 
     @method_decorator(user_passes_test(lambda u: u.is_authenticated))
     def dispatch(self, *args, **kwargs):
