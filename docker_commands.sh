57725133008bad9cab8017b72067362031fbd287
revike <revike@ya.ru>
2022-03-09 22:12:08 +0300

    index

--- docker_commands.sh
+++ docker_commands.sh
@@ -5,6 +5,6 @@ python3 manage.py migrate --database mongodb
 python3 manage.py create_superuser
 python3 manage.py create_categories
 python3 manage.py collectstatic --noinput
-sleep 20
+sleep 5
 python3 manage.py search_index --rebuild -f
 python3 manage.py runserver 0.0.0.0:8000
