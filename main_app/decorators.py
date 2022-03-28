67de76f4937b1b2b69fb3b106add9bd2ead047ee
Evgenii Fedorin <revike@ya.ru>
2022-03-28 13:15:35 +0300

    update_at time see product

--- main_app/decorators.py
+++ main_app/decorators.py
@@ -1,19 +1,19 @@
 from functools import wraps
-
-from django.utils.datetime_safe import datetime
-
+from django.utils import timezone
 from main_app.models import PageHit
 
 
 def counted(func):
     """Декоратор для подсчета просмотра продукта"""
+
     @wraps(func)
     def wrapper(request, *args, **kwargs):
         page_hit = PageHit.objects.filter(url=request.request.path)
         if page_hit.count():
             page_count = page_hit.first().count
-            page_hit.update(count=page_count+1, updated=datetime.now())
+            page_hit.update(count=page_count + 1, updated=timezone.now())
         else:
             page_hit.create(url=request.request.path)
         return func(request, *args, **kwargs)
+
     return wrapper
